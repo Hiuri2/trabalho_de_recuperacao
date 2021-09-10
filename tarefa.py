77fc87a2f8e703f49635d47239c8c8ba552034e1
Hiuri2 <89426466+Hiuri2@users.noreply.github.com>
2021-09-10 20:19:43 -0300

    Update tarefa.py

--- tarefa.py
+++ tarefa.py
@@ -1,9 +1,9 @@
-  
 class Tarefa:
 
-    def _init_(self):
-        self.__descricao = ""
-        self.__status = ""
+    def __init__(self):
+        self.__descricao = ''
+        self.__status = ''
+
         self.set_status_pendente()
 
     def get_descricao(self):
@@ -12,17 +12,12 @@ class Tarefa:
     def set_descricao(self, descricao):
         self.__descricao = descricao
 
-    def get_descricao(self):
-        return self.__descricao
-
-    def set_contato(self, contato):
-        self.__contato = contato
-
     def get_status(self):
         return self.__status
 
     def set_status_concluida(self):
-        self.__status = "concluida"
+        self.__status = 'Concluida'
 
     def set_status_pendente(self):
-        self.__status = "Pendente"
+        self.__status = 'Pendente'
+
